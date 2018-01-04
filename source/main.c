ad1270de302580383b3dd1031c937c73202c96b3
chrisvm <christian.velez55@gmail.com>
2018-01-04 00:32:18 -0400

    added more input printing

--- source/main.c
+++ source/main.c
@@ -1,10 +1,38 @@
 #include <3ds.h>
 #include <stdio.h>
 
+void print_inputs(u32 kDown, u32 kHold)
+{
+    // print touchpad
+    touchPosition touch;
+    hidTouchRead(&touch);
+    printf("\x1b[8;0HX:%03d, Y:%03d", touch.px, touch.py);
+
+    // print analog
+    circlePosition circlePos;
+    hidCircleRead(&circlePos);
+    printf("\x1b[9;0HX:%03d, Y:%03d ", circlePos.dx, circlePos.dy);
+
+    u32 downAndHold = kDown | kHold;
+
+    if (downAndHold & KEY_A) {
+        printf("\x1b[10;0H\x1b[32mA Button");
+    } else {
+        printf("\x1b[10;0H\x1b[37mA Button");
+    }
+
+    if (downAndHold & KEY_B) {
+        printf("\x1b[11;0H\x1b[32mB Button");
+    } else {
+        printf("\x1b[11;0H\x1b[37mB Button");
+    }
+
+    printf("\x1b[37m");
+}
 
 int main()
 {
-	// Initializations
+	// Initialization
 	srvInit();        // services
 	aptInit();        // applets
 	hidInit();        // input
@@ -12,6 +40,7 @@ int main()
 	gfxSet3D(false);  // stereoscopy (true: enabled / false: disabled)
 
 	u32 kDown;        // keys down
+    u32 kHold;
 
 	consoleInit(GFX_TOP, NULL);
 
@@ -23,25 +52,18 @@ int main()
 	printf("Screen Height: %u\n", fb_height);
 	printf("\x1b[21;16HPress Start to exit.");
 
-	touchPosition touch;
-	circlePosition circlePos;
-
 	// Main loop
 	while (aptMainLoop()) {
 		// Read which buttons are currently pressed or not
 		hidScanInput();
 
-		// print touchpad
-		hidTouchRead(&touch);
-		printf("\x1b[8;0HX:%03d, Y:%03d", touch.px, touch.py);
-
-		// print analog
-		hidCircleRead(&circlePos);
-		printf("\x1b[9;0HX:%03d, Y:%03d", circlePos.dx, circlePos.dy);
-
 		// If START button is pressed, break loop and quit
 		kDown = hidKeysDown();
+        kHold = hidKeysHeld();
+
 		if (kDown & KEY_START) break;
+
+        print_inputs(kDown, kHold);
 
 		// Flush and swap framebuffers
 		gfxFlushBuffers();
